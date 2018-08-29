09e6424605e5c30e9ca816a3bbe8616f8a539ebe
Felipe Melo <narulee@hotmail.com>
2018-08-29 14:30:33 -0300

    update

--- tp1.c
+++ tp1.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <string.h>
 
 // .h
 
@@ -13,8 +15,12 @@ typedef struct lista Lista;
 typedef struct filme Filme;
 
 Lista* inicializaLista();
-int insereFilme(Lista* li, struct filme f);
-void liberaLista(Lista* li)
+int insereFilme(Lista* li, Filme f);
+void liberaLista(Lista* li);
+void consultaTitulo(Lista *li, char* title);
+void aluga(Filme *f);
+void imprimeFilme();
+int converteChar(char c);
 
 // .c
 
@@ -23,7 +29,7 @@ void liberaLista(Lista* li)
 struct lista {
   int qtd;
   struct filme dados[MAX];
-}
+};
 
 Lista* inicializaLista(){
   Lista* li = (Lista*)malloc(sizeof(struct lista));
@@ -49,29 +55,65 @@ int insereFilme(Lista* li, struct filme f){
 void consultaTitulo(Lista *li, char* title){
   if (li == NULL)
     return 0;
-  int i = 0;
-  while (i<li->qtd && li->dados[i].titulo != *title)
-    i++;
-  if (i == li->qtd)
-    return 0;  //nao foi encontrado o elemento
-  *title = li->dados[i];
-  return 1;
-}
+
+  if (strcmp(li->dados[i].titulo, title) == 0){
+    printf("%s, %d", li->dados[i].titulo, li->dados[i].ano);
+  }
+
+  return 0;  //nao foi encontrado o elemento
 
 void aluga(Filme *f){
   if (f != NULL)
     f->locacao++;
 }
 
-void imprimeFilme()
+void imprimeFilme(){
+
+}
+
+int convertChar(char c){
+  return (c - 48);
+}
+
+int convertString(char* ano){
+  for (int j, expoente=i; j <= i; j++){
+    year += convertChar(ano[j]) * pow(10, expoente);
+    expoente --;
+  }
+  return year;
+}
+
 
 // main.c
 
 int main(void){
+
+  int virgula;
   FILE *arq;
   arq = fopen("filmes.txt", "r");
+  char c;
+  char ano[10];
+  int i;
+  int year=0;
+  Filme f;
 
-  while (!feof(arq)){
-    while ()
+  while(1) {
+
+    for (i=0, c = fgetc(arq); c != ','; i++)
+      f.titulo[i] = fgetc(arq);
+
+    for (i=0, c = fgetc(arq); c != '\n'; i++)
+      ano[i] = fgetc(arq);
+
+    f.ano = convertString(ano);
+
+    if (feof(arq) != 0)
+      break;
+
+    /*if (c == ','){
+      virgula = i;
+      printf("%d", virgula);
+      break;*/
+    }
   }
 }
